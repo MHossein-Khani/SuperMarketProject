cb577ca968bcb18a77058600f954caf980aafc66
MHossein <mhkhan95@gmil.com>
2022-05-05 17:29:20 +0430

    Add Application Db context

---
+++ src/SuperMarket.Persistance.EF/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Persistance.EF
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly
+                (typeof(ApplicationDbContext).Assembly);
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<SalesInvoice> SalesInvoices { get; set; }
+        public DbSet<PurchaseInvoice> PurchaseInvoices { get; set; }
+
+    }
+}
