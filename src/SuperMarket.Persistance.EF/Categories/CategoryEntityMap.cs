98d41b54cd732d27ecbc050f20ebeb6fc54e5f06
MHossein <mhkhan95@gmil.com>
2022-05-05 16:53:46 +0430

    Add Category entity map

---
+++ src/SuperMarket.Persistance.EF/Categories/CategoryEntityMap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Persistance.EF.Categories
+{
+    public class CategoryEntityMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+
+            builder.ToTable("Categories");
+
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+
+            builder.Property(p => p.Name).IsRequired();
+
+        }
+    }
+}
