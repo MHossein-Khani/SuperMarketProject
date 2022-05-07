2ed47be13829bed4c9e2e1356253fe08fd72feea
MHossein <mhkhan95@gmil.com>
2022-05-07 17:29:37 +0430

    Add add product spec test layer

---
+++ src/SuperMarket.Specs/Products/AddProduct.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using SuperMarket.Entities;
+using SuperMarket.Infrastructure.Application;
+using SuperMarket.Infrastructure.Test;
+using SuperMarket.Persistance.EF;
+using SuperMarket.Specs.Infrastructure;
+using SuperMarket.Test.Tools;
+using System.Linq;
+using Xunit;
+using static SuperMarket.Specs.BDDHelper;
+
+namespace SuperMarket.Specs.Products
+{
+    public class AddProduct : EFDataContextDatabaseFixture
+    {
+    }
+}
