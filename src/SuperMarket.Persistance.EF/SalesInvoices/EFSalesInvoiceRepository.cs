825367f854d54c738ec7c178e26df9d71f71fe34
MHossein <mhkhan95@gmil.com>
2022-05-08 11:29:37 +0430

    Add repository and ef repository and service and app service of sales invoice to project

---
+++ src/SuperMarket.Persistance.EF/SalesInvoices/EFSalesInvoiceRepository.cs
@@ -0,0 +1,21 @@
+﻿using SuperMarket.Services.SalesInvoices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Persistance.EF.SalesInvoices
+{
+    public class EFSalesInvoiceRepository : SalesInvoiceRepository
+    {
+        private readonly EFDataContext _dataContext;
+
+        public EFSalesInvoiceRepository(EFDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+
+    }
+}
---
+++ src/SuperMarket.Services/SalesInvoices/SalesInvoiceAppService.cs
@@ -0,0 +1,22 @@
+﻿using SuperMarket.Infrastructure.Application;
+using SuperMarket.Services.SalesInvoices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket.Services.SalesInvoices
+{
+    public class SalesInvoiceAppService : SalesInvoiceService
+    {
+        private readonly SalesInvoiceRepository _repository;
+        private readonly UnitOfWork _unitOfWork;
+
+        public SalesInvoiceAppService(SalesInvoiceRepository repository, UnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
