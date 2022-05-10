41fd3840c1d0c2b4509a4180f47c4cebfb911331
MHossein <mhkhan95@gmil.com>
2022-05-11 00:24:33 +0430

    Add purchase invoice controller

---
+++ src/SuperMarket.RestAPI/Controllers/PurchaseInvoicesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperMarket.Services.PurchaseInvoices.Contracts;
+using System.Collections.Generic;
+
+namespace SuperMarket.RestAPI.Controllers
+{
+    [Route("api/purchaseInvoice")]
+    [ApiController]
+    public class PurchaseInvoicesController : Controller
+    {
+        private readonly PurchaseInvoiceService _service;
+
+        public PurchaseInvoicesController(PurchaseInvoiceService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public void Add(AddPurchaseInvoiceDto dto)
+        {
+             _service.Add(dto);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _service.Delete(id);
+        }
+
+        [HttpGet]
+        public List<GetPurchaseInvoiceDto> GetAll()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpPatch("{id}")]
+        public void Update(UpdatePurchaseInvoiceDto dto, [FromRoute]int id)
+        {
+            _service.Update(dto, id);
+        }
+    }
+}
