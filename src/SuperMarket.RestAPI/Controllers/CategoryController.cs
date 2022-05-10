fce4069f748498e9961522cdd3bd32e75abc4d6f
MHossein <mhkhan95@gmil.com>
2022-05-11 00:11:38 +0430

    Add controller for category

---
+++ src/SuperMarket.RestAPI/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperMarket.Services.Categories.Contracts;
+using System.Collections.Generic;
+
+namespace SuperMarket.RestAPI.Controllers
+{
+    [Route("api/catgeories")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly CategoryService _service;
+
+        public CategoryController(CategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public void Add(AddCategoryDto dto)
+        {
+            _service.Add(dto);
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            _service.Delete(id);
+        }
+
+        [HttpGet]
+        public List<GetCategoryDto> GetAll()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpPatch("{id}")]
+        public void Update(UpdateCategoryDto dto, [FromRoute]int id)
+        {
+            _service.Update(dto, id);
+        }
+    }
+}
